d387264c5af2fe0b8e6470d1d90a72306fbcf9fb
Stu Basden <stu@tapiocacollective.com>
2012-06-06 14:42:37 -0400

    added renaming script for mis-named courses

---
+++ renamePagesBasedOnTitle.php
@@ -0,0 +1,129 @@
+<?php
+$title = 'Rename Pages based on their Title';
+
+// $type_override = 'page';
+$start_asset = '502accc57f00000217772487eb1a5242';
+
+// Optionally override the container/child types
+// $asset_type = 'assetFactoryContainer';
+// $asset_children_type = 'assetFactory';
+
+function pagetest($child) {
+  if (preg_match('/[A-Z]/', $child->path->path))
+    return true;
+}
+function foldertest($child) {
+  return false;
+}
+function edittest($asset) {
+  return true;
+}
+
+function changes(&$asset) {
+  /* If you wish to use $changed, make sure it's global, and set it to false. 
+   * When something is changed, it becomes true: */
+  // global $changed;
+  // $changed = false;
+  // if ($asset["metadata"]->teaser != 'test') {$changed = true;}
+  // $asset["metadata"]->teaser = 'test';
+}
+
+
+include('html_header.php');
+
+?>
+
+
+<?php
+
+function readFolder($client, $auth, $id) {
+  $folder = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($folder->readReturn->success == 'true') {
+    $asset = ( array ) $folder->readReturn->asset->folder;
+    if ($_POST['folder'] == 'on') {
+      echo "<h1>Folder: ".$asset["path"]."</h1>";
+    }
+    if ($_POST['children'] == 'on') {
+      echo '<input type="checkbox" class="hidden" id="Aexpand'.$asset['id'].'"><label class="fullpage" for="Aexpand'.$asset['id'].'">';
+        print_r($asset["children"]); // Shows all the children of the folder
+      echo '</label>';
+    }
+    indexFolder($client, $auth, $asset);
+  } else {
+    echo '<div class="f">Failed to read folder: '.$asset["path"].'</div>';
+  }
+}
+function indexFolder($client, $auth, $asset) {
+  if (!is_array($asset["children"]->child)) {
+    $asset["children"]->child=array($asset["children"]->child);
+  }
+  foreach($asset["children"]->child as $child) {
+    if ($child->type == "page") {
+      if (pagetest($child))
+        readPage($client, $auth, array ('type' => 'page', 'id' => $child->id));
+    } elseif ($child->type == "folder") {
+      if (foldertest($child))
+        readFolder($client, $auth, array ('type' => 'folder', 'id' => $child->id));
+    }
+  }
+}
+
+function readPage($client, $auth, $id) {
+  $reply = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($reply->readReturn->success == 'true') {
+    $asset = ( array ) $reply->readReturn->asset->page;
+    if ($_POST['asset'] == 'on') {
+      echo '<h4>'.$asset['path']."</h4>";
+    }
+    
+    if (edittest($asset)) {
+      editPage($client, $auth, $asset);
+    }
+    
+  } else {
+    echo '<div class="f">Failed to read page: '.$id.'</div>';
+  }
+}
+
+
+function editPage($client, $auth, $asset) {
+  global $total, $start_asset;
+  echo '<div class="page">';
+  if ($_POST['before'] == 'on') {
+    echo '<input type="checkbox" class="hidden" id="Bexpand'.$asset['id'].'"><label class="fullpage" for="Bexpand'.$asset['id'].'">';
+      print_r($asset); // Shows the page in all its glory
+    echo '</label>';
+  }
+  
+  echo "<script type='text/javascript'>var page_".$asset['id']." = ";
+  print_r(json_encode($asset));
+  echo '; console.log(page_'.$asset['id'].')';
+  echo "</script>";
+  
+  changes($asset);
+  
+  if ($_POST['after'] == 'on') {
+    echo '<input type="checkbox" class="hidden" id="Aexpand'.$asset['id'].'"><label class="fullpage" for="Aexpand'.$asset['id'].'">';
+      print_r($asset); // Shows the page as it will be
+    echo '</label>';
+  }
+  
+  $page_title = strtolower(str_replace(':','',str_replace(' ','-',$asset['metadata']->title)));
+  echo $page_title;
+  if ($_POST['action'] == 'edit') {
+    $move = $client->move ( array ('authentication' => $auth, 'identifier' => array('type' => 'page', 'id' => $asset["id"]), 'moveParameters' => array('newName'=> $page_title, 'doWorkflow'=>false) ) );
+  }
+  
+  
+  if ($move->moveReturn->success == 'true') {
+    echo '<div class="s">Edit success</div>';
+    $total['s']++;
+  } else {
+    echo '<div class="f">Edit failed: '.$asset['path'].'<div>'.extractMessage($result).'</div></div>';
+    $total['f']++;
+  }
+  echo '</div>';
+}
+
+
+?>
