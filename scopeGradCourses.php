9928def135a6a2d2ec789e166b7237f4cb0bce7f
Stu Basden <stu@tapiocacollective.com>
2012-08-07 10:29:58 -0400

    added simple script, scopeGradCourses.php

---
+++ scopeGradCourses.php
@@ -0,0 +1,43 @@
+<?php
+$title = 'Change the courses scope to Graduate';
+
+// $type_override = 'page';
+$start_asset = '7cd6f38f7f0000024411971cf97ea0c3';
+
+// Optionally override the container/child types
+// $asset_type = 'assetFactoryContainer';
+// $asset_children_type = 'assetFactory';
+
+function pagetest($child) {
+  if (preg_match('/[a-z]/', $child->path->path))
+    return true;
+}
+function foldertest($child) {
+  if (preg_match('/[a-z]/', $child->path->path))
+    return true;
+}
+function edittest($asset) {
+  if (preg_match('/[a-z]/', $asset["contentTypePath"]))
+    return true;
+}
+
+function changes(&$asset) {
+  /* If you wish to use $changed, make sure it's global, and set it to false. 
+   * When something is changed, it becomes true: */
+  global $changed;
+  $changed = false;
+  // if ($asset["metadata"]->teaser != 'test') {$changed = true;}
+  foreach ($asset["metadata"]->dynamicFields->dynamicField as $dyn) {
+    if ($dyn->name == "scope") {
+      if ($dyn->fieldValues->fieldValue->value != 'Graduate') {
+        $changed = true;
+        $dyn->fieldValues->fieldValue->value = 'Graduate';
+      }
+    }
+  }
+}
+
+
+include('header.php');
+
+?>
