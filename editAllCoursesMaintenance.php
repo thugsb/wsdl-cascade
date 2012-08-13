4cf6da4d05e8235ee1e161d7f40894eb8ab35c2b
Stu Basden <stu@tapiocacollective.com>
2012-08-13 17:40:36 -0400

    fix for older files where the faculty-set wasn't an array, and made sure the title is escaped (& <)

--- editAllCoursesMaintenance.php
+++ editAllCoursesMaintenance.php
@@ -38,11 +38,14 @@ function edittest($asset) {
 }
 
 function changes(&$asset) {
-  global $changed;
+  global $changed, $total;
   $changed = false;
-  if ($asset["metadata"]->title != trim($asset['metadata']->title)) {
+  $newTitle = trim($asset['metadata']->title);
+  $newTitle = preg_replace('/& /','&amp; ',$newTitle);
+  $newTitle = preg_replace('/< /','&lt; ',$newTitle);
+  if ($asset["metadata"]->title != $newTitle) {
     $changed = true;
-    $asset['metadata']->title = trim($asset['metadata']->title);
+    $asset['metadata']->title = $newTitle;
   }
   foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $field) {
     if ($field->identifier == 'description') {
@@ -55,11 +58,15 @@ function changes(&$asset) {
       $max = count($field->structuredDataNodes->structuredDataNode)-1;
       if ($max > 5) { /* If you want to allow more faculty, make sure to to add the fields to the metadata set and then up these numbers to match */
         $max = 5;
+        $total['f']++;
         echo '<div class="f">There are too many faculty connections for the metadata to take.</div>';
       }
       for ($i = 0;$i <= $max; $i++) {
         // echo 'Type: '.gettype($field->structuredDataNodes->structuredDataNode[$i]).'<br>';
         // print_r($field->structuredDataNodes->structuredDataNode[$i]->structuredDataNodes->structuredDataNode);
+        if(!is_array($field->structuredDataNodes->structuredDataNode[$i]->structuredDataNodes->structuredDataNode)) {
+          $field->structuredDataNodes->structuredDataNode[$i]->structuredDataNodes->structuredDataNode = array($field->structuredDataNodes->structuredDataNode[$i]->structuredDataNodes->structuredDataNode);
+        }
         foreach ($field->structuredDataNodes->structuredDataNode[$i]->structuredDataNodes->structuredDataNode as $subfield) {
           if ($subfield->identifier == 'faculty') {
             if ($asset["metadata"]->dynamicFields->dynamicField[2*$i+18]->fieldValues->fieldValue->value != 'site://'.str_replace(':','/',$subfield->pagePath)) {$changed = true;}
