4d13934018c2dcfed068f1cf21d61138681ae050
Stu Basden <stu@tapiocacollective.com>
2012-08-14 16:39:32 -0400

    added script to read the metadata set of folders (and save it into a file)

---
+++ readFolderMetadataSet.php
@@ -0,0 +1,139 @@
+<?php
+$title = 'Check to see if the folder have the correct metadata set';
+
+// $type_override = 'page';
+$start_asset = 'c621c0d17f00000101f92de5212d40b7';
+
+// Optionally override the container/child types
+// $asset_type = 'assetFactoryContainer';
+// $asset_children_type = 'assetFactory';
+
+function pagetest($child) {
+  // if (preg_match('/[a-z]/', $child->path->path))
+  return false;
+}
+function foldertest($child) {
+  return true;
+}
+function edittest($asset) {
+  // if (preg_match('/[a-z]/', $asset["contentTypePath"]))
+  return false;
+}
+
+function changes(&$asset) {
+  /* If you wish to use $changed, make sure it's global, and set it to false. 
+   * When something is changed, it becomes true: */
+  // global $changed;
+  // $changed = false;
+  // if ($asset["metadata"]->teaser != 'test') {$changed = true;}
+  // $asset["metadata"]->teaser = 'test';
+}
+
+
+include('html_header.php');
+
+?>
+
+
+<?php
+
+function readFolder($client, $auth, $id) {
+  $folder = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($folder->readReturn->success == 'true') {
+    $asset = ( array ) $folder->readReturn->asset->folder;
+    if ($_POST['folder'] == 'on') {
+      echo "<h1>Folder: ".$asset["path"]."</h1>";
+    }
+    
+    if ($asset['metadataSetPath'] != 'www_config:Default Sets/Site Section') {
+      $folderLink = '<a class="left_label" href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&amp;type=folder">'.$asset['siteName'].'://'.$asset["path"].'</a> '.$asset['metadataSetPath']."<br>\n";
+      echo $folderLink;
+      if ($_POST['action'] == 'edit') {
+        $myFile = "_ref/metaDataSets.html";
+        $fh = fopen($myFile, 'a') or die("can't open file");
+        fwrite($fh, $folderLink);
+        fclose($fh);
+      }
+    }
+    if ($_POST['children'] == 'on') {
+      echo '<input type="checkbox" class="hidden" id="Aexpand'.$asset['id'].'"><label class="fullpage" for="Aexpand'.$asset['id'].'">';
+        print_r($asset["children"]); // Shows all the children of the folder
+      echo '</label>';
+    }
+    indexFolder($client, $auth, $asset);
+  } else {
+    echo '<div class="f">Failed to read folder: '.$asset["path"].'</div>';
+  }
+}
+function indexFolder($client, $auth, $asset) {
+  if (!is_array($asset["children"]->child)) {
+    $asset["children"]->child=array($asset["children"]->child);
+  }
+  foreach($asset["children"]->child as $child) {
+    if ($child->type == "page") {
+      if (pagetest($child))
+        readPage($client, $auth, array ('type' => 'page', 'id' => $child->id));
+    } elseif ($child->type == "folder") {
+      if (foldertest($child))
+        readFolder($client, $auth, array ('type' => 'folder', 'id' => $child->id));
+    }
+  }
+}
+
+function readPage($client, $auth, $id) {
+  $reply = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($reply->readReturn->success == 'true') {
+    $asset = ( array ) $reply->readReturn->asset->page;
+    if ($_POST['asset'] == 'on') {
+      echo '<h4>'.$asset['path']."</h4>";
+    }
+    
+    if (edittest($asset)) {
+      editPage($client, $auth, $asset);
+    }
+    
+  } else {
+    echo '<div class="f">Failed to read page: '.$id.'</div>';
+  }
+}
+
+
+function editPage($client, $auth, $asset) {
+  global $total;
+  echo '<div class="page">';
+  if ($_POST['before'] == 'on') {
+    echo '<input type="checkbox" class="hidden" id="Bexpand'.$asset['id'].'"><label class="fullpage" for="Bexpand'.$asset['id'].'">';
+      print_r($asset); // Shows the page in all its glory
+    echo '</label>';
+  }
+  
+  echo "<script type='text/javascript'>var page_".$asset['id']." = ";
+  print_r(json_encode($asset));
+  echo '; console.log(page_'.$asset['id'].')';
+  echo "</script>";
+  
+  changes($asset);
+  
+  if ($_POST['after'] == 'on') {
+    echo '<input type="checkbox" class="hidden" id="Aexpand'.$asset['id'].'"><label class="fullpage" for="Aexpand'.$asset['id'].'">';
+      print_r($asset); // Shows the page as it will be
+    echo '</label>';
+  }
+  
+  if ($_POST['action'] == 'edit') {
+    $edit = $client->edit ( array ('authentication' => $auth, 'asset' => array('page' => $asset) ) );
+  }
+  
+  
+  if ($edit->editReturn->success == 'true') {
+    echo '<div class="s">Edit success</div>';
+    $total['s']++;
+  } else {
+    echo '<div class="f">Edit failed: '.$asset['path'].'<div>'.extractMessage($result).'</div></div>';
+    $total['f']++;
+  }
+  echo '</div>';
+}
+
+
+?>
