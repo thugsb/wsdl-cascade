9b966692ff404326d2ec2ce533da9a4e5d733080
Stu Basden <stu@tapiocacollective.com>
2012-08-27 11:17:39 -0400

    Added script to grab <H2> text from wysiwyg content

---
+++ addDisplayNameFromContentH2.php
@@ -0,0 +1,51 @@
+<?php
+$title = 'Add a Display Name to pages that do not have them, from the WYSIWYG content H2s';
+
+// $type_override = 'page';
+$start_asset = '3beb3e397f00000209340e79b7d2e67f';
+
+// Optionally override the container/child types
+// $asset_type = 'assetFactoryContainer';
+// $asset_children_type = 'assetFactory';
+
+function pagetest($child) {
+  if (preg_match('/[a-z]/', $child->path->path))
+    return true;
+}
+function foldertest($child) {
+  if (preg_match('/^[a-z]/', $child->path->path))
+    return true;
+}
+function edittest($asset) {
+  if (preg_match('/[a-z]/', $asset["contentTypePath"]))
+    return true;
+}
+
+function changes(&$asset) {
+  global $changed;
+  $changed = false;
+  
+  if (!is_array($asset["structuredData"]->structuredDataNodes->structuredDataNode))
+  $asset["structuredData"]->structuredDataNodes->structuredDataNode = array ($asset["structuredData"]->structuredDataNodes->structuredDataNode);
+  
+  foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $field) {
+    if ($field->identifier == 'content') {
+      if ($asset["metadata"]->displayName == '') {
+        $changed = true;
+        $matches = array();
+        preg_match('/<h2\b[^>]*>(.*?)<\/h2>/', $field->text, $matches);
+        $h2 = strip_tags($matches[0]);
+        echo '<div>'.$h2.'</div>';
+        $asset["metadata"]->displayName = $h2;
+      } else {
+        $changed = false;
+        echo '<div>Display Name: '.$asset["metadata"]->displayName.'</div>';
+      }
+    }
+  }
+}
+
+
+include('header.php');
+
+?>
