0d1de22804e65fa68aeca574ad480106b2b8ceed
Stu Basden <stu@tapiocacollective.com>
2013-01-07 07:50:41 -0500

    adding scripts for magazine site conversion

---
+++ mag.php
@@ -0,0 +1,176 @@
+<?php
+$title = 'Test';
+
+// $type_override = 'page';
+$start_asset = 'af0a5a337f0000020fc8e91301f5d7aa';
+
+// Optionally override the container/child types
+// $asset_type = 'assetFactoryContainer';
+// $asset_children_type = 'assetFactory';
+
+function pagetest($child) {
+  if (preg_match('/[a-z]/', $child->path->path))
+    return true;
+}
+function foldertest($child) {
+  return false;
+}
+function edittest($asset) {
+  if (preg_match('/[a-z]/', $asset["contentTypePath"]))
+    return true;
+}
+
+function changes(&$asset) {
+  global $changed;
+  $changed = false;
+  
+  include('/Users/stu/Sites/www.slc.edu/magazine/'.$asset['path'].'.html');
+  echo $title;
+  
+  if ($asset['metadata']->title != $title) {
+    $asset['metadata']->title = $title;
+    $changed = true;
+  }
+  if ($asset['metadata']->displayName != '') {
+    $asset['metadata']->displayName = '';
+    $changed = true;
+  }
+  
+  foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $group) {
+    if ($group->identifier == 'main_column') {
+      foreach ($group->structuredDataNodes->structuredDataNode as $field) {
+        if ($field->identifier == 'content') {
+          if ($field->text != strip_tags($content,'<h1><h2><h3><h4><h5><h6><img><em><strong><p><a>')) {
+            $field->text = strip_tags($content,'<h1><h2><h3><h4><h5><h6><img><em><strong><p><a>');
+            $changed = true;
+          }
+        }
+      }
+    }
+    if ($group->identifier == 'right_sidebar') {
+      foreach ($group->structuredDataNodes->structuredDataNode as $field) {
+        if ($field->identifier == 'content') {
+          if ($field->text != strip_tags($right,'<h1><h2><h3><h4><h5><h6><img><em><strong><p><a>')) {
+            $field->text = strip_tags($right,'<h1><h2><h3><h4><h5><h6><img><em><strong><p><a>');
+            $changed = true;
+          }
+        }
+        if ($field->identifier == 'include' && trim($right) != '') {
+          if ($field->text != 'Yes') {
+            $field->text = 'Yes';
+            $changed = true;
+          }
+        }
+      }
+    }
+  }
+  
+}
+
+
+include('html_header.php');
+
+?>
+
+
+<?php
+
+function readFolder($client, $auth, $id) {
+  global $asset_type, $asset_children_type, $data, $o, $cron;
+  $folder = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($folder->readReturn->success == 'true') {
+    
+    $asset = ( array ) $folder->readReturn->asset->$asset_type;
+    if ($_POST['folder'] == 'on') {
+      echo "<h1>Folder: ".$asset["path"]."</h1>";
+    }
+    if ($_POST['children'] == 'on') {
+      echo '<input type="checkbox" class="hidden" id="Aexpand'.$asset['id'].'"><label class="fullpage" for="Aexpand'.$asset['id'].'">';
+        print_r($asset["children"]); // Shows all the children of the folder
+      echo '</label>';
+    }
+    indexFolder($client, $auth, $asset);
+  } else {
+    echo '<div class="f">Failed to read folder: '.$asset["path"].'</div>';
+  }
+}
+function indexFolder($client, $auth, $asset) {
+  global $asset_type, $asset_children_type, $data, $o, $cron;
+  if (!is_array($asset["children"]->child)) {
+    $asset["children"]->child=array($asset["children"]->child);
+  }
+  foreach($asset["children"]->child as $child) {
+    if ($child->type == strtolower($asset_children_type)) {
+      if (pagetest($child))
+        readPage($client, $auth, array ('type' => $child->type, 'id' => $child->id), $child->type);
+    } elseif ($child->type === strtolower($asset_type)) {
+      if (foldertest($child))
+        readFolder($client, $auth, array ('type' => $child->type, 'id' => $child->id));
+    }
+  }
+}
+
+function readPage($client, $auth, $id, $type) {
+  global $asset_type, $asset_children_type, $data, $o, $cron;
+  $reply = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($reply->readReturn->success == 'true') {
+    $asset = ( array ) $reply->readReturn->asset->$asset_children_type;
+    if ($_POST['asset'] == 'on') {
+      $name = '';
+      if (!$asset['path']) {$name = $asset['name'];}
+      echo '<h4><a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type='.$type.'#highlight">'.$asset['path'].$name."</a></h4>";
+    }
+    
+    if (edittest($asset)) {
+      echo '<div class="page">';
+      if ($_POST['before'] == 'on') {
+        echo '<input type="checkbox" class="hidden" id="Bexpand'.$asset['id'].'"><label class="fullpage" for="Bexpand'.$asset['id'].'">';
+          print_r($asset); // Shows the page in all its glory
+        echo '</label>';
+      }
+
+      echo "<script type='text/javascript'>var page_".$asset['id']." = ";
+      print_r(json_encode($asset));
+      echo '; console.log(page_'.$asset['id'].')';
+      echo "</script>";
+      
+      editPage($client, $auth, $asset);
+      echo '</div>';
+    }
+    
+  } else {
+    echo '<div class="f">Failed to read page: '.$id.'</div>';
+  }
+}
+
+
+function editPage($client, $auth, $asset) {
+  global $total, $asset_type, $asset_children_type, $data, $changed, $o, $cron;
+  
+  changes($asset);
+  
+  if ($_POST['after'] == 'on') {
+    echo '<input type="checkbox" class="hidden" id="Aexpand'.$asset['id'].'"><label class="fullpage" for="Aexpand'.$asset['id'].'">';
+      print_r($asset); // Shows the page as it will be
+    echo '</label>';
+  }
+  
+  if ($changed == true) {
+    if ($_POST['action'] == 'edit') {
+      $edit = $client->edit ( array ('authentication' => $auth, 'asset' => array($asset_children_type => $asset) ) );
+    }
+    if ($edit->editReturn->success == 'true') {
+      echo '<div class="s">Edit success</div>';
+      $total['s']++;
+    } else {
+      echo '<div class="f">Edit failed: '.$asset['path'].'<div>'.extractMessage($result).'</div></div>';
+      $total['f']++;
+    }
+  } else {
+    echo '<div class="k">No changes needed</div>';
+    $total['k']++;
+  }
+}
+
+
+?>
