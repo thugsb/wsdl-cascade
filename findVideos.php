b9b3078792baa770e80fd23e9516a7ce83a207b3
Stu Basden <stu@tapiocacollective.com>
2013-06-06 09:18:44 -0400

    find blip.tv too and count properly

--- findVideos.php
+++ findVideos.php
@@ -9,10 +9,18 @@ $title = 'Just read all pages, looking for ones with videos';
 // $asset_type = 'assetFactoryContainer';
 // $asset_children_type = 'assetFactory';
 
+function preg_match_count($pattern, $input) {
+  if(preg_match_all($pattern, $input, $matches, PREG_PATTERN_ORDER)) {
+    return count($matches[0]);
+  }
+  return 0;
+}
+
 function pagetest($child) {
     return true;
 }
 function foldertest($child) {
+  if (!preg_match('/^events\/2012/', $child->path->path))
     return true;
 }
 function edittest($asset) {
@@ -23,10 +31,10 @@ function changes(&$asset) {
   global $changed;
   $changed = false;
   $text = print_r($asset, true);
-  if (preg_match('/edgecast/',$text) ) {
+  if (preg_match('/edgecast/si',$text) or preg_match('/blip/si',$text) ) {
     $name = '';
     if (!$asset['path']) {$name = $asset['name'];}
-    echo '<h4><a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type=page#highlight">'.$asset['path'].$name." (".preg_match('/edgecast/',$text)." matches)</a></h4>";
+    echo '<h4><a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type=page#highlight">'.$asset['path'].$name." (".(preg_match_count('/edgecast/si',$text)+preg_match_count('/blip/si',$text))." matches)</a></h4>";
     
   }
 }
