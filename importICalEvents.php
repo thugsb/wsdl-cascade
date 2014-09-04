3873123ab98d9ed78ff7c9bba557854e1e0a7062
Stu Basden <stu@tapiocacollective.com>
2014-09-04 16:21:49 -0400

    iCAL importer: convert date to UNIX timestamp

--- importICalEvents.php
+++ importICalEvents.php
@@ -1,6 +1,36 @@
 <?php
+date_default_timezone_set('America/New_York');
 
 if (!isset($_GET['user'])) {exit;}
+
+// From https://code.google.com/p/ics-parser/source/browse/trunk/class.iCalReader.php
+function iCalDateToUnixTimestamp($icalDate) 
+    { 
+        $icalDate = str_replace('T', '', $icalDate); 
+        $icalDate = str_replace('Z', '', $icalDate); 
+
+        $pattern  = '/([0-9]{4})';   // 1: YYYY
+        $pattern .= '([0-9]{2})';    // 2: MM
+        $pattern .= '([0-9]{2})';    // 3: DD
+        $pattern .= '([0-9]{0,2})';  // 4: HH
+        $pattern .= '([0-9]{0,2})';  // 5: MM
+        $pattern .= '([0-9]{0,2})/'; // 6: SS
+        preg_match($pattern, $icalDate, $date); 
+
+        // Unix timestamp can't represent dates before 1970
+        if ($date[1] <= 1970) {
+            return false;
+        } 
+        // Unix timestamps after 03:14:07 UTC 2038-01-19 might cause an overflow
+        // if 32 bit integers are used.
+        $timestamp = mktime((int)$date[4], 
+                            (int)$date[5], 
+                            (int)$date[6], 
+                            (int)$date[2],
+                            (int)$date[3], 
+                            (int)$date[1]);
+        return  $timestamp;
+    }
 
 // Function found at http://evertpot.com/248/ (modified, see below)
 function iCalendarToXML($icalendarData) {
@@ -66,7 +96,13 @@ function iCalendarToXML($icalendarData) {
             }
 
             // This line modified to remove escaping-backslashes
-            $xml.='>'. str_replace("\\",'',htmlspecialchars($value)) . '</' . $propertyName . ">\n";
+            $xml.='>';
+            if ($propertyName == 'DTSTART' || $propertyName == 'DTEND' || $propertyName == 'DTSTAMP') {
+              $xml.=iCalDateToUnixTimestamp($value);
+            } else {
+              $xml.=str_replace("\\",'',htmlspecialchars($value));
+            }
+            $xml.='</' . $propertyName . ">\n";
             // Original: $xml.='>'. htmlspecialchars($value) . '</' . $propertyName . ">\n";
 
         }
