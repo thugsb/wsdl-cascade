d509c9f68625581c36d26dc0777edc103c347230
Stu Basden <stu@tapiocacollective.com>
2015-02-13 17:48:13 -0500

    Add a catalogue script to delete previous years' index pages

---
+++ catalogue/deletePreviousYearIndexPages.php
@@ -0,0 +1,158 @@
+<?php
+date_default_timezone_set('America/New_York');
+$title = 'DELETE previous year index pages';
+
+$start_asset = '817373157f00000101f92de5bea1554a';
+
+$year_to_delete = "/2014-2015/";
+
+function pagetest($child) {
+  global $year_to_delete;
+  if (preg_match($year_to_delete, $child->path->path))
+    return true;
+}
+function foldertest($child) {
+  preg_match('/\//', $child->path->path, $matches);
+  if (count($matches) < 3) {
+    return true;
+  }
+}
+function edittest($asset) {
+  if (preg_match('/[a-z]/', $asset["contentTypePath"]))
+    return true;
+}
+
+function changes(&$asset) {
+  /* If you wish to use $changed, make sure it's global, and set it to false. 
+   * When something is changed, it becomes true: */
+  global $changed;
+  $changed = false;
+  // if ($asset["metadata"]->teaser != 'test') {
+  //    $changed = true;
+  //    $asset["metadata"]->teaser = 'test';
+  // }
+}
+
+if (!$cron) {include('../html_header.php');}
+
+
+
+function readFolder($client, $auth, $id) {
+  global $asset_type, $asset_children_type, $data, $o, $cron;
+  $folder = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($folder->readReturn->success == 'true') {
+    
+    $asset = ( array ) $folder->readReturn->asset->$asset_type;
+    if ($cron) {
+      $o[4] .= "<h4>Folder: ".$asset["path"]."</h4>";
+    } elseif ($_POST['folder'] == 'on') {
+      echo "<h1>Folder: ".$asset["path"]."</h1>";
+    }
+    if ($_POST['children'] == 'on' && !$cron) {
+      echo '<button class="btn" href="#cModal'.$asset['id'].'" data-toggle="modal">View Children</button><div id="cModal'.$asset['id'].'" class="modal hide" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-body">';
+        print_r($asset["children"]); // Shows all the children of the folder
+      echo '</div></div>';
+    }
+    indexFolder($client, $auth, $asset);
+  } else {
+    if ($cron) {
+      $o[1] .= '<div style="padding:3px;color:#fff;background:#c00;">Failed to read folder: '.$asset["path"].'</div>';
+    } else {
+      echo '<div class="f">Failed to read folder: '.$asset["path"].'</div>';
+    }
+  }
+}
+function indexFolder($client, $auth, $asset) {
+  global $asset_type, $asset_children_type, $data, $o, $cron;
+  if (!is_array($asset["children"]->child)) {
+    $asset["children"]->child=array($asset["children"]->child);
+  }
+  foreach($asset["children"]->child as $child) {
+    if ($child->type == strtolower($asset_children_type)) {
+      if (pagetest($child))
+        readPage($client, $auth, array ('type' => $child->type, 'id' => $child->id), $child->type);
+    } elseif ($child->type === strtolower($asset_type)) {
+      if (foldertest($child))
+        readFolder($client, $auth, array ('type' => $child->type, 'id' => $child->id));
+    }
+  }
+}
+
+function readPage($client, $auth, $id, $type) {
+  global $asset_type, $asset_children_type, $data, $o, $cron;
+  $reply = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($reply->readReturn->success == 'true') {
+    // For some reason the names of asset differ from the returned object
+    $returned_type = '';
+    foreach ($reply->readReturn->asset as $t => $a) {
+      if (!empty($a)) {$returned_type = $t;}
+    }
+    
+    $asset = ( array ) $reply->readReturn->asset->$returned_type;
+    if ($cron) {
+      $o[3] .= '<h4><a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type='.$type.'#highlight">'.$asset['path']."</a></h4>";
+    } elseif ($_POST['asset'] == 'on') {
+      $name = '';
+      if (!$asset['path']) {$name = $asset['name'];}
+      echo '<h4><a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type='.$type.'#highlight">'.$asset['path'].$name."</a></h4>";
+    }
+    
+    if (edittest($asset)) {
+      if (!$cron) {echo '<div class="page">';}
+      if ($_POST['before'] == 'on' && !$cron) {
+        echo '<button class="btn" href="#bModal'.$asset['id'].'" data-toggle="modal">View Before</button><div id="bModal'.$asset['id'].'" class="modal hide" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-body">';
+          print_r($asset); // Shows the page in all its glory
+        echo '</div></div>';
+      }
+
+      if (!$cron) {
+        echo "<script type='text/javascript'>var page_".$asset['id']." = ";
+        print_r(json_encode($asset));
+        echo '; console.log(page_'.$asset['id'].')';
+        echo "</script>";
+      }
+      
+      editPage($client, $auth, $asset);
+      if (!$cron) {echo '</div>';}
+    }
+    
+  } else {
+    if ($cron) {
+      $o[1] .= '<div style="padding:3px;color:#fff;background:#c00;">Failed to read page: '.$id.'</div>';
+    } else {
+      echo '<div class="f">Failed to read page: '.$id.'</div>';
+    }
+  }
+}
+
+
+function editPage($client, $auth, $asset) {
+  global $total, $asset_type, $asset_children_type, $data, $changed, $o, $cron;
+  
+  if ($_POST['action'] == 'edit' || $cron) {
+    $delete = $client->delete ( array ('authentication' => $auth, 'identifier' => array ('type' => $asset_children_type, 'id' => $asset['id'] ) ) );
+    $name = '';
+    if (!$asset['path']) {$name = $asset['name'];}
+    
+    if ($delete->deleteReturn->success == 'true') {
+      if ($cron) {
+        $o[2] .= $name.' was deleted<br>';
+      } else {
+        echo '<div class="s">'.$asset['path'].$name.' was deleted</div>';
+      }
+      $total['s']++;
+    } else {
+      if ($cron) {
+        $o[1] .= $name.' FAILED to delete<br>';
+      } else {
+        echo '<div class="f">'.$asset['path'].$name.' could not be deleted</div>';
+      }
+      print_r($delete);
+      $total['f']++;
+    }
+  }
+}
+
+?>
+
+
