7f874a2cf8d544f7dd307001169b0ce68729023f
Stu Basden <stu@tapiocacollective.com>
2015-09-03 13:50:02 -0600

    first working version of the migration script

--- migration/migrateData.php
+++ migration/migrateData.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('America/New_York');
-$title = 'Test';
+$title = 'Migrate data from old DD to new via interim DD';
 
 // $type_override = 'page';
 $start_asset = '898fea49c0a8022b3b519fac2af23643';
@@ -9,16 +9,41 @@ $start_asset = '898fea49c0a8022b3b519fac2af23643';
 // $asset_type = 'assetFactoryContainer';
 // $asset_children_type = 'assetFactory';
 
+function createNode($type, $id, $nodes = false, $text = '', $at=false, $bi=false, $bp=false, $fi=false, $fp=false, $pi=false, $pp=false, $si=false, $sp=false, $re=false) {
+	$newnode = new StdClass();
+	$newnode->type = $type;
+	$newnode->identifier = $id;
+	if ($nodes == true) {
+		$newnode->structuredDataNodes = new StdClass();
+		$newnode->structuredDataNodes->structuredDataNode = array();
+	} else {
+		$newnode->structuredDataNodes = false;
+	}
+	$newnode->text = $text;
+	if ($at) {$newnode->assetType = $at;}
+	if ($at) {$newnode->blockId = $bi;}
+	if ($at) {$newnode->blockPath = $bp;}
+	if ($at) {$newnode->fileId = $fi;}
+	if ($at) {$newnode->filePath = $fp;}
+	if ($at) {$newnode->pageId = $pi;}
+	if ($at) {$newnode->pagePath = $pp;}
+	if ($at) {$newnode->symlinkId = $si;}
+	if ($at) {$newnode->symlinkPath = $sp;}
+	if ($at) {$newnode->recycled = $re;}
+	return $newnode;
+}
+
+
 function pagetest($child) {
   if (preg_match('/[a-z]/', $child->path->path))
     return true;
 }
 function foldertest($child) {
-  if (preg_match('/^_[a-z]/', $child->path->path))
+  if (preg_match('/^[a-z]/', $child->path->path))
     return true;
 }
 function edittest($asset) {
-  if (preg_match('/[a-z]/', $asset["contentTypePath"]))
+  if (preg_match('/tpl-default-interim/', $asset["contentTypePath"]))
     return true;
 }
 
@@ -26,72 +51,217 @@ function changes(&$asset) {
   /* If you wish to use $changed, make sure it's global, and set it to false. 
    * When something is changed, it becomes true: */
   global $changed, $total;
-  $changed = true;
-  foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $sdnode) {
-    if ($sdnode->identifier == "main_column") {
-  		foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
-  			if ($subnode->identifier == 'syndicate-from') {
-  				if ($subnode->pageId != '') {echo "<div class='f'>Syndicate From is selected.</div>"; $total['f']++;}
-  			}
-  			if ($subnode->identifier == 'data-definition-block') {
-  				if ($subnode->blockId != '') {
-  				
-  					foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $ssdnode) {
-  						if ($ssdnode->identifier == "primary") {
-  							foreach ($ssdnode->structuredDataNodes->structuredDataNode as $ssubnode) {
-  								if ($ssubnode->identifier == 'type') {
-										$newnode = new StdClass();
-										$newnode->identifier = 'primary';
-										$newnode->structuredDataNodes = new StdClass();
-										$newnode->structuredDataNodes->structuredDataNode = array();
-										$newnode->structuredDataNodes->structuredDataNode[0]->identifier = 'type';
-										$newnode->structuredDataNodes->structuredDataNode[0]->type = 'text';
-										$newnode->structuredDataNodes->structuredDataNode[0]->text = 'External Block';
-										$newnode->structuredDataNodes->structuredDataNode[1]->identifier = 'external';
-										$newnode->structuredDataNodes->structuredDataNode[1]->type = 'group';
-										$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes = new StdClass();
-										$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode = array();
-										$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0]->identifier = '';
-										$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0]->type = 'asset';
-										$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0]->assetType = 'block';
-										$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0]->blockId = $ssubnode->blockId;
- 										array_push($sdnode, $newnode);
- 										echo "<div class='f'>External Block needs a type, and check its placement.</div>";
-  								}
-  							}
-  						}
-  					}
-  					
-  				}
-  			}
-  			if ($subnode->identifier == 'content') {
-  				if ($subnode->text != '') {
-  			
-  					foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $ssdnode) {
-  						if ($ssdnode->identifier == "intro") {
-  							foreach ($ssdnode->structuredDataNodes->structuredDataNode as $ssubnode) {
-  								if ($ssubnode->identifier == 'text') {
-  									$ssubnode->text = $subnode->text;
-  									echo "<div class='s'>WYSIWYG content copied into Intro.</div>";
-  								}
-  							}
-  						}
-  					}
-  				
-  				}
-  			}
-  			
-  		}
-    }
-  }
+  if ($asset['contentTypeId'] == '899184bec0a8022b3b519fac8a850c42') {
+		$changed = true;
 
-  // if ($asset["metadata"]->teaser != 'test') {
-  //    $changed = true;
-  //    $asset["metadata"]->teaser = 'test';
-  // }
+
+		$newnode = createNode('text', 'data', false, 'tpl-default');
+		array_unshift($asset["structuredData"]->structuredDataNodes->structuredDataNode, $newnode);
+	
+	
+		$settings = createNode('group', 'group-settings', true);
+		$foundPageHeading = false;
+		foreach ($asset["metadata"]->dynamicFields->dynamicField as $meta) {
+			if ($meta->name = 'page-heading') {
+				$foundPageHeading = true;
+				if ($meta->fieldValues->fieldValue->value == '') {
+					$settings->structuredDataNodes->structuredDataNode[0] = createNode('text', 'page-heading', false, 'Display Name');
+				} else {
+					$settings->structuredDataNodes->structuredDataNode[0] = createNode('text', 'page-heading', false, 'Custom');
+				}
+				$settings->structuredDataNodes->structuredDataNode[1] = createNode('text', 'custom-page-heading', false, $meta->fieldValues->fieldValue->value);
+			}
+		}
+		if (!$foundPageHeading) {echo "<div class='f'>Didn't find a page heading.</div>";}
+		$settings->structuredDataNodes->structuredDataNode[2] = createNode('asset', 'image', false, '', 'file');
+
+	
+	
+		foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $sdnode) {
+			if ($sdnode->identifier == "main_column") {
+				foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+				
+					if ($subnode->identifier == 'syndicate-from') {
+						if ($subnode->pageId != '') {echo "<div class='f'>Syndicate From is selected.</div>"; $total['f']++;}
+					}
+				
+					if ($subnode->identifier == 'data-definition-block') {
+						if ($subnode->blockId != '') {
+					
+							foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $ssdnode) {
+								if ($ssdnode->identifier == "group-primary") {
+									foreach ($ssdnode->structuredDataNodes->structuredDataNode as $ssubnode) {
+										if ($ssubnode->identifier == 'type') {
+											$newnode = createNode('group', 'group-primary', true);
+											$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'status', false, 'On');
+											$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'type', false, 'External Block');
+											$newnode->structuredDataNodes->structuredDataNode[1] = createNode('group', 'group-block', true);
+											$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0] = createNode('asset', 'block', false, '', 'block', $ssubnode->blockId);
+											array_push($asset["structuredData"]->structuredDataNodes->structuredDataNode, $newnode);
+											echo "<div class='k'>External Block needs a type, and check its placement.</div>";
+											$primaryOn = true;
+										}
+									}
+								}
+							}
+						
+						}
+					}
+				
+					if ($subnode->identifier == 'content') {
+						if ($subnode->text != '') {
+							$newnode = createNode('group', 'group-primary', true);
+							$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'status', false, ($righton ? 'On': 'Off'));
+							$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'type', false, 'Text');
+							$newnode->structuredDataNodes->structuredDataNode[1] = createNode('group', 'group-text', true);
+							$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0] = createNode('text', 'wysiwyg', false, $subnode->text);
+							array_push($asset["structuredData"]->structuredDataNodes->structuredDataNode, $newnode);
+							echo "<div class='s'>Main Column WYSIWYG content will be copied into Primary.</div>";
+							$primaryOn = true;
+						}
+					}
+				
+				}
+			}
+		
+			if ($sdnode->identifier == "left_sidebar") {
+				$left = false; $lefton = false;
+				foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+					if ($subnode->identifier == 'include') {
+						if ($subnode->text == 'Yes') {
+							$lefton = true;
+						}
+					}
+					if ($subnode->identifier == 'content') {
+						if ($subnode->text != '') {
+							$left = true;
+						}
+					}
+				}
+				if ($left) {
+					foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+						if ($subnode->identifier == 'content') {
+							$newnode = createNode('group', 'group-nav', true);
+							if ($lefton) {
+								$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'status', false, 'On');
+							} else {
+								$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'status', false, 'Off');
+							}
+							$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'type', false, 'Text');
+							$newnode->structuredDataNodes->structuredDataNode[1] = createNode('group', 'group-text', true);
+							$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0] = createNode('text', 'wysiwyg', false, $subnode->text);
+							array_push($asset["structuredData"]->structuredDataNodes->structuredDataNode, $newnode);
+							echo "<div class='s'>Left Sidebar content will be copied into Nav.</div>";
+							$navOn = true;
+						}
+					}
+				}
+			}
+		
+			if ($sdnode->identifier == "right_sidebar") {
+				$right = false; $righton = false;
+				foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+					if ($subnode->identifier == 'include') {
+						if ($subnode->text == 'Yes') {
+							$righton = true;
+						}
+					}
+					if ($subnode->identifier == 'content') {
+						if ($subnode->text != '') {
+							$right = true;
+						}
+					}
+				}
+				if ($right) {
+					foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+						if ($subnode->identifier == 'content') {
+							$newnode = createNode('group', 'group-secondary', true);
+							$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'status', false, ($righton ? 'On': 'Off'));
+							$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'type', false, 'Text');
+							$newnode->structuredDataNodes->structuredDataNode[1] = createNode('group', 'group-text', true);
+							$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0] = createNode('text', 'wysiwyg', false, $subnode->text);
+							array_push($asset["structuredData"]->structuredDataNodes->structuredDataNode, $newnode);
+							echo "<div class='s'>Right Sidebar content will be copied into Secondary.</div>";
+							$secondaryOn = true;
+						}
+					}
+				}
+			}
+		
+			if ($sdnode->identifier == "forms") {
+				$form = false;
+				if (!is_array($sdnode->structuredDataNodes->structuredDataNode)) {
+					$sdnode->structuredDataNodes->structuredDataNode = array($sdnode->structuredDataNodes->structuredDataNode);
+				}
+				foreach ($sdnode->structuredDataNodes->structuredDataNode as $form) {
+					$formOn = false; $formID = ''; $formType = ''; $formRe = ''; $formPos = '';
+					foreach ($form->structuredDataNodes->structuredDataNode as $subnode) {
+						if ($subnode->identifier == 'placement') {
+							if ($subnode->text != 'Off') {
+								$formOn = true;
+								$formPos = $subnode->text;
+								if ($subnode->text == 'Manual' || $subnode->text == 'Right Sidebar') {echo "<div class='k'>Form used to be in $subnode->text, but will be placed in primary.</div>";}
+							}
+						}
+						if ($subnode->identifier == 'type') {
+							$formType = $subnode->text;
+						}
+						if ($subnode->identifier == 'id') {
+							$formID = $subnode->text;
+						}
+						if ($subnode->identifier == 'redirect') {
+							$formRe = $subnode->text;
+						}
+					}
+					if ($formID != '') {
+						$newnode = createNode('group', 'group-primary', true);
+						$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'status', false, ($formOn ? 'On' : 'Off') );
+						$newnode->structuredDataNodes->structuredDataNode[0] = createNode('text', 'type', false, 'Form');
+						$newnode->structuredDataNodes->structuredDataNode[1] = createNode('group', 'group-form', true);
+						$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0] = createNode('text', 'type', false, $formType);
+						$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0] = createNode('text', 'id', false, $formID);
+						$newnode->structuredDataNodes->structuredDataNode[1]->structuredDataNodes->structuredDataNode[0] = createNode('text', 'redirect', false, $formRe);
+						array_push($asset["structuredData"]->structuredDataNodes->structuredDataNode, $newnode);
+						echo "<div class='k'>Form will be copied into Primary. It was previously in $formPos, so check its placement.</div>";
+						$primaryOn = true;
+					}
+				}
+			}
+		
+		
+			if ($sdnode->identifier == "related") {
+				foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+					if ($subnode->identifier == 'include') {
+						if ($subnode->text == 'Yes') {echo "<div class='f'>WARNING: Related Content is included. Fix this manually.</div>";}
+					}
+				}
+			}
+			if ($sdnode->identifier == "ssp") {
+				foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+					if ($subnode->identifier == 'embed') {
+						if ($subnode->text == 'Yes') {echo "<div class='f'>WARNING: SSP is included. Fix this manually.</div>";}
+					}
+				}
+			}
+			if ($sdnode->identifier == "gallery") {
+				foreach ($sdnode->structuredDataNodes->structuredDataNode as $subnode) {
+					if ($subnode->identifier == 'size') {
+						if ($subnode->text == 'Big' || $subnode->text == 'Small') {echo "<div class='f'>WARNING: Gallery is included. Its size if $subnode->text. Fix this manually.</div>";}
+					}
+				}
+			}
+		}
+
+		$settings->structuredDataNodes->structuredDataNode[3] = createNode('text', 'intro', false, ($introOn ? '::CONTENT-XML-CHECKBOX::On' : '::CONTENT-XML-CHECKBOX::'));
+		$settings->structuredDataNodes->structuredDataNode[4] = createNode('text', 'nav', false, ($navOn ? '::CONTENT-XML-CHECKBOX::On' : '::CONTENT-XML-CHECKBOX::'));
+		$settings->structuredDataNodes->structuredDataNode[5] = createNode('text', 'primary', false, ($primaryOn ? '::CONTENT-XML-CHECKBOX::On' : '::CONTENT-XML-CHECKBOX::'));
+		$settings->structuredDataNodes->structuredDataNode[6] = createNode('text', 'secondary', false, ($secondaryOn ? '::CONTENT-XML-CHECKBOX::On' : '::CONTENT-XML-CHECKBOX::'));
+
+		array_push($asset["structuredData"]->structuredDataNodes->structuredDataNode, $settings);
+	}
 }
 
-if (!$cron) {include('..html_header.php');}
+if (!$cron) {include('../html_header.php');}
 
 
 
