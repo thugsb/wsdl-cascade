44cf3be598ebc0f739168fafbfb09de031b96873
Stu Basden <stu@tapiocacollective.com>
2015-09-24 13:44:43 -0400

    cron tweak2

--- news-events/importEventsToPending.php
+++ news-events/importEventsToPending.php
@@ -44,7 +44,7 @@ $start_asset = $year_folder;
 
 $message = 'Set ?from=yyyy-mm-dd&to=yyyy-mm-dd ';
 
-mail('stu@t.apio.ca','Events Import Script Started',date('D, d M Y H:i'));
+if (mail('stu@t.apio.ca','Events Import Script Started',date('D, d M Y H:i'))) {echo 'mail sent';} else {echo 'mail error';}
 
 function pagetest($child) {
   // if (preg_match('/[a-z]/', $child->path->path))
