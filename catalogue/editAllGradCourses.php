6f941362b78b59229e04b6235f1449567611aa4b
Stu Basden <stu@tapiocacollective.com>
2016-03-16 17:11:25 +0100

    Catalogue: Update the grad year

--- catalogue/editAllGradCourses.php
+++ catalogue/editAllGradCourses.php
@@ -18,7 +18,7 @@ $start_asset = '4e9e12a97f000001015d84e03ea3fb26';
  */
 
 // $year = '[-0-9]+'; // Matches all years
-$year = '2015-2016';
+$year = '2016-2017';
 
 function pagetest($child) {
   global $year;
