c8fc952efec9f55bb4e42feb5f071f7d51d3e121
Stu Basden <stu@tapiocacollective.com>
2016-05-18 13:34:02 +0200

    Add script to display Cincopa galleries

---
+++ includeCincopaGallery.php
@@ -0,0 +1,39 @@
+<?php
+
+// $cincopaID = 'A0JA9WN5fdRp';
+
+if (!isset($cincopaID)) {exit;}
+
+$curl = curl_init();
+curl_setopt ($curl, CURLOPT_URL, 'http://www.cincopa.com/media-platform/runtimeze/json.aspx?details=all&fid='.$cincopaID);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec ($curl);
+curl_close ($curl);
+
+$json = json_decode( $result );
+
+foreach ($json->items as $i => $item) {
+	$desc = '';
+	if ($item->description != '') {
+		$desc = "<figcaption><p>$item->description</p></figcaption>";
+	}
+	$output = <<<EOT
+<div class='gallery-item'>
+	<a class='image link-exp link-exp-lbx'>
+		<img alt='$item->title' src='$item->thumbnail_url'/>
+		<div class='icon-holder'><div class='icon i-exp-link' data-grunticon-embed='data-grunticon-embed'></div></div>
+		<div class='cpt-lightbox' id='cincopaID$item->id'>
+			<div class='field-image'>
+				<img alt='$item->title' src='$item->content_url'>
+			</div>
+			$desc
+		</div>
+	</a>
+</div>
+EOT;
+echo $output;
+}
+
+// echo '<pre>';
+// print_r( $json );
+// echo '</pre>';
