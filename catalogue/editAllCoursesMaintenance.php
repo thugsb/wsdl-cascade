71eaf239933ccf6ce58d2319e6163e7902e3aff8
Stu Basden <stu@tapiocacollective.com>
2016-06-28 11:44:17 +0200

    Add Discipline display names and folder paths to courses as metadata

--- catalogue/editAllCoursesMaintenance.php
+++ catalogue/editAllCoursesMaintenance.php
@@ -38,7 +38,7 @@ function edittest($asset) {
 }
 
 function changes(&$asset) {
-  global $changed, $total, $discNames, $relatedIDs, $year, $auth, $client, $cron, $o;
+  global $changed, $total, $discNames, $relatedIDs, $year, $auth, $client, $cron, $o, $disc_folder, $disc_name;
   $changed = false;
   $newTitle = trim($asset['metadata']->title);
   $newTitle = preg_replace('/& /','and ',$newTitle);
@@ -47,6 +47,22 @@ function changes(&$asset) {
   if ($asset["metadata"]->title != $newTitle) {
     $changed = true;
     $asset['metadata']->title = $newTitle;
+  }
+  if ( isset($disc_folder) ) {
+    foreach ($asset["metadata"]->dynamicFields->dynamicField as $dyn) {
+      if ($dyn->name == "discipline-folder") {
+        if ($dyn->fieldValues->fieldValue->value != $disc_folder) {
+          $dyn->fieldValues->fieldValue->value = $disc_folder;
+          $changed = true;
+        }
+      }
+      if ($dyn->name == "discipline-name") {
+        if ($dyn->fieldValues->fieldValue->value != $disc_folder) {
+          $dyn->fieldValues->fieldValue->value = $disc_name;
+          $changed = true;
+        }
+      }
+    }
   }
   foreach ($asset["structuredData"]->structuredDataNodes->structuredDataNode as $field) {
     if ($field->identifier == 'description') {
@@ -186,7 +202,142 @@ include('relatedIDs.php');
 
 
 if (!$cron) {
-  include('../header.php');
+  include('../html_header.php');
 }
+
+
+
+function readFolder($client, $auth, $id) {
+  global $asset_type, $asset_children_type, $data, $o, $cron, $disc_folder, $disc_name;
+  $folder = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($folder->readReturn->success == 'true') {
+    
+    $asset = ( array ) $folder->readReturn->asset->$asset_type;
+    if ($cron) {
+      $o[4] .= "<h4>Folder: ".$asset["path"]."</h4>";
+    } elseif ($_POST['folder'] == 'on') {
+      echo "<h1>Folder: ".$asset["path"]."</h1>";
+    }
+    if ($_POST['children'] == 'on' && !$cron) {
+      echo '<button class="btn" href="#cModal'.$asset['id'].'" data-toggle="modal">View Children</button><div id="cModal'.$asset['id'].'" class="modal hide" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-body">';
+        print_r($asset["children"]); // Shows all the children of the folder
+      echo '</div></div>';
+    }
+    if ( !strpos($asset['path'], '/') ) {
+      $disc_name = $asset['metadata']->displayName;
+      $disc_folder = $asset['path'];
+    }
+    indexFolder($client, $auth, $asset);
+  } else {
+    if ($cron) {
+      $o[1] .= '<div style="padding:3px;color:#fff;background:#c00;">Failed to read folder: '.$asset["path"].'</div>';
+    } else {
+      echo '<div class="f">Failed to read folder: '.$asset["path"].'</div>';
+    }
+  }
+}
+function indexFolder($client, $auth, $asset) {
+  global $asset_type, $asset_children_type, $data, $o, $cron, $total;
+  if (!is_array($asset["children"]->child)) {
+    $asset["children"]->child=array($asset["children"]->child);
+  }
+  foreach($asset["children"]->child as $child) {
+    if ($child->type == strtolower($asset_children_type)) {
+      if (pagetest($child))
+        readPage($client, $auth, array ('type' => $child->type, 'id' => $child->id), $child->type);
+    } elseif ($child->type === strtolower($asset_type)) {
+      if (foldertest($child))
+        readFolder($client, $auth, array ('type' => $child->type, 'id' => $child->id));
+    }
+  }
+}
+
+function readPage($client, $auth, $id, $type) {
+  global $asset_type, $asset_children_type, $data, $o, $cron;
+  $reply = $client->read ( array ('authentication' => $auth, 'identifier' => $id ) );
+  if ($reply->readReturn->success == 'true') {
+    // For some reason the names of asset differ from the returned object
+    $returned_type = '';
+    foreach ($reply->readReturn->asset as $t => $a) {
+      if (!empty($a)) {$returned_type = $t;}
+    }
+    
+    $asset = ( array ) $reply->readReturn->asset->$returned_type;
+    if ($cron) {
+      $o[3] .= '<h4><a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type='.$type.'#highlight">'.$asset['path']."</a></h4>";
+    } elseif ($_POST['asset'] == 'on') {
+      $name = '';
+      if (!$asset['path']) {$name = $asset['name'];}
+      echo '<h4><a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type='.$type.'#highlight">'.$asset['path'].$name."</a></h4>";
+    }
+    
+    if (edittest($asset)) {
+      if (!$cron) {echo '<div class="page">';}
+      if ($_POST['before'] == 'on' && !$cron) {
+        echo '<button class="btn" href="#bModal'.$asset['id'].'" data-toggle="modal">View Before</button><div id="bModal'.$asset['id'].'" class="modal hide" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-body">';
+          print_r($asset); // Shows the page in all its glory
+        echo '</div></div>';
+      }
+
+      if (!$cron) {
+        echo "<script type='text/javascript'>var page_".$asset['id']." = ";
+        print_r(json_encode($asset));
+        echo '; console.log(page_'.$asset['id'].')';
+        echo "</script>";
+      }
+      
+      editPage($client, $auth, $asset);
+      if (!$cron) {echo '</div>';}
+    }
+    
+  } else {
+    if ($cron) {
+      $o[1] .= '<div style="padding:3px;color:#fff;background:#c00;">Failed to read page: '.$id.'</div>';
+    } else {
+      echo '<div class="f">Failed to read page: '.$id.'</div>';
+    }
+  }
+}
+
+
+function editPage($client, $auth, $asset) {
+  global $total, $asset_type, $asset_children_type, $data, $changed, $o, $cron;
+  
+  changes($asset);
+  
+  if ($_POST['after'] == 'on' && !$cron) {
+    echo '<button class="btn" href="#aModal'.$asset['id'].'" data-toggle="modal">View After</button><div id="aModal'.$asset['id'].'" class="modal hide" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-body">';
+      print_r($asset); // Shows the page as it will be
+    echo '</div></div>';
+  }
+  
+  if ($changed == true) {
+    if ($_POST['action'] == 'edit') {
+      $edit = $client->edit ( array ('authentication' => $auth, 'asset' => array($asset_children_type => $asset) ) );
+    }
+    if ($edit->editReturn->success == 'true') {
+      if ($cron) {
+        $o[2] .= '<div style="color:#090;">Edit success: <a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type='.$type.'#highlight">'.$asset['path']."</a></div>";
+      } else {
+        echo '<div class="s">Edit success</div>';
+      }
+      $total['s']++;
+    } else {
+      if ($_POST['debug'] == 'on') {
+        $result = $client->__getLastResponse();
+      }
+      if ($cron) {
+        $o[1] .= '<div style="padding:3px;color:#fff;background:#c00;">Edit failed: <a href="https://cms.slc.edu:8443/entity/open.act?id='.$asset['id'].'&type='.$type.'#highlight">'.$asset['path']."</a><div>".htmlspecialchars(extractMessage($result)).'</div></div>';
+      } else {
+        echo '<div class="f">Edit failed: '.$asset['path'].'<div>'.htmlspecialchars(extractMessage($result)).'</div></div>';
+      }
+      $total['f']++;
+    }
+  } else {
+    if (!$cron) {echo '<div class="k">No changes needed</div>';}
+    $total['k']++;
+  }
+}
+
 
 ?>
