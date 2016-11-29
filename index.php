a894dc7e0fab470597515b924c1abe73f1b34c8e
Stu Basden <stu@tapiocacollective.com>
2016-11-29 10:00:08 +0000

    index now displays file list on localhost

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 echo $_SERVER['HTTPS'];
+
+$whitelist = array(
+    '127.0.0.1', // localhost
+    '::1' // IPv6 localhost
+);
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+	foreach (new DirectoryIterator(".") as $fn) {
+		$name = $fn->getFilename();
+    	echo "<div><a href='$name'>$name</a></div>";
+	}
+}
+
 ?>--
