cfc7735f448b8dc0fe9e76bc79626eef86db2ad3
Stu Basden <stu@tapiocacollective.com>
2017-05-22 17:44:43 +0100

    add scripts that look for files on the server and check if they exist in Cascade, creating index files of missing ones

---
+++ cleanup/event-ics.php
@@ -0,0 +1,54 @@
+<?php
+date_default_timezone_set('America/New_York');
+$title = 'Test the existence of pages in Cascade that are related to *.ics files found on the server';
+
+$lines = file('../indexes/stu-ics-list.txt', FILE_IGNORE_NEW_LINES);
+
+
+// $type_override = 'page';
+// $start_asset = 'e59cc45a7f00000100c46dcf503b2144';
+
+// Optionally override the container/child types
+// $asset_type = 'assetFactoryContainer';
+// $asset_children_type = 'assetFactory';
+
+function pagetest($child) {
+  if (preg_match('/[a-z]/', $child->path->path))
+    return true;
+}
+function foldertest($child) {
+  if (preg_match('/^_[a-z]/', $child->path->path))
+    return true;
+}
+function edittest($asset) {
+  if (preg_match('/[a-z]/', $asset["contentTypePath"]))
+    return true;
+}
+
+function changes(&$asset) {
+  /* If you wish to use $changed, make sure it's global, and set it to false. 
+   * When something is changed, it becomes true: */
+  global $changed;
+  $changed = false;
+}
+
+if (!$cron) {include('../header.php');}
+
+foreach ($lines as $path) {
+  $cascadePath = preg_replace('/\.ics$/', '', substr($path, 14) );
+  $reply = $client->read ( array ('authentication' => $auth, 'identifier' => array(type => 'page', path => array( path => $cascadePath, siteName => 'www.sarahlawrence.edu+news-events') ) ) );
+  // echo '<pre>';
+  // print_r($reply);
+  // echo '</pre>';
+  if ($reply->readReturn->success == 'true') {
+    echo '<div class="s">true</div> '.$cascadePath.'<br>';
+    $total['s']++;
+
+  } else {
+    echo '<div class="f">false</div> '.$cascadePath.'<br>';
+    $total['f']++;
+  }
+}
+
+
+?>
---
+++ cleanup/media.php
@@ -0,0 +1,171 @@
+<?php
+date_default_timezone_set('America/New_York');
+$title = 'Test the existence of Media files in Cascade that are related to files found on the server';
+
+$lines = file('../indexes/stu-media-list.txt', FILE_IGNORE_NEW_LINES);
+
+
+// $type_override = 'page';
+$start_asset = 'None';
+
+// Optionally override the container/child types
+// $asset_type = 'assetFactoryContainer';
+// $asset_children_type = 'assetFactory';
+
+function pagetest($child) {
+  return false;
+}
+function foldertest($child) {
+  return false;
+}
+function edittest($asset) {
+  return false;
+}
+
+function changes(&$asset) {
+  global $changed;
+  $changed = false;
+}
+
+if (!$cron) {include('../header.php');}
+
+$publishableFolders = [];
+$unpublishableFolders = [];
+
+$read = 0;
+$publishable = 0;
+$ok = 0;
+$unpub = 0;
+$absent = 0;
+$ignored = 0;
+$inUnpubFolder = 0;
+file_put_contents('../indexes/stu-media-unpublishable.txt', "");
+file_put_contents('../indexes/stu-media-unpublishable-folder.txt', "");
+file_put_contents('../indexes/stu-media-missing.txt', "");
+
+foreach ($lines as $path) {
+  $cascadePath = substr($path, 2);
+
+  // if ( preg_match('/^(faculty-bak|media|communications)\//', $cascadePath) ) {
+  if ( preg_match('/^about\/visit\/360\//', $cascadePath) ) {
+    $ignored++;
+    continue;
+  }
+
+  if (preg_match('/^news-events\//', $cascadePath) ) {
+    $siteName = 'www.sarahlawrence.edu+news-events';
+    $cascadePath = substr($cascadePath, 12);
+    echo '<div class="k">Reading '.$cascadePath.' in '.$siteName.'</div>';
+  } elseif (preg_match('/^magazine\//', $cascadePath) ) {
+    $siteName = 'www.sarahlawrence.edu+magazine';
+    $cascadePath = substr($cascadePath, 9);
+    echo '<div class="k">Reading '.$cascadePath.' in '.$siteName.'</div>';
+  } else {
+    $siteName = 'SarahLawrence.edu';
+    echo '<div class="k">Reading '.$cascadePath.'</div>';
+  }
+
+  $reply = $client->read ( array ('authentication' => $auth, 'identifier' => array(type => 'file', path => array( path => $cascadePath, siteName => $siteName) ) ) );
+  // echo '<pre>';
+  // print_r($reply);
+  // echo '</pre>';
+
+  
+  if ($reply->readReturn->success == 'true') {
+    $asset = ( array ) $reply->readReturn->asset->file;
+    $read++;
+    if ( $asset['shouldBePublished'] == 1 ) {
+      $publishable++;
+      $parentFolders = split( '/', $cascadePath );
+      $filename = array_pop($parentFolders);
+      $allAncestorFoldersArePublishable = true;
+      foreach($parentFolders as $folder) {
+        if (preg_match('/^'.$folder.'\//', $cascadePath)) {
+          $folderPath = $folder;
+        } else {
+          $parents = explode('/'.$folder.'/', $cascadePath);
+          $folderPath = $parents[0].'/'.$folder;
+        }
+        // echo '<div>'.$folderPath.'</div>';
+
+        if ( in_array($folderPath, $publishableFolders) ) {
+          // Do nothing. All is fine, so continue thru the foreach.
+          // echo '<div class="s">publishable</div>';
+        } elseif ( in_array($folderPath, $unpublishableFolders) ) {
+          // File is in an unpublishable folder
+          file_put_contents('../indexes/stu-media-unpublishable-folder.txt', $folderPath.' in '.$cascadePath."\n", FILE_APPEND);
+          echo '<div class="f">The '.$folderPath.' folder is unpublishable.</div>';
+          $inUnpubFolder++;
+          $allAncestorFoldersArePublishable = false;
+        } else {
+          echo '<div class="k">Reading '.$folderPath.'</div>';
+          $reply = $client->read ( array ('authentication' => $auth, 'identifier' => array(type => 'folder', path => array( path => $folderPath, siteName => $siteName) ) ) );
+          if ($reply->readReturn->success == 'true') {
+            if ( $reply->readReturn->asset->folder->shouldBePublished ) {
+              array_push($publishableFolders, $folderPath);
+            } else {
+              array_push($unpublishableFolders, $folderPath);
+              // File is in an unpublishable folder
+              file_put_contents('../indexes/stu-media-unpublishable-folder.txt', $folderPath.' in '.$cascadePath."\n", FILE_APPEND);
+              echo '<div class="f">The '.$folderPath.' folder is unpublishable.</div>';
+              $inUnpubFolder++;
+              $allAncestorFoldersArePublishable = false;
+            }
+          } else {
+            echo '<div class="f">Folder cannot be read: '.$folderPath.' in '.$cascadePath.'</div>';
+            print_r($reply);
+          }
+        }
+      }
+      if ( $allAncestorFoldersArePublishable ) {
+        $ok++;
+        echo '<div class="s">All is OK</div> '.$cascadePath.'<br>';
+      }
+
+    } else {
+      // File exists in Cascade but isn't publishable
+      file_put_contents('../indexes/stu-media-unpublishable.txt', $cascadePath."\n", FILE_APPEND);
+      echo '<div class="f">The '.$cascadePath.' Media is unpublishable, yet is published.</div>';
+      $unpub++;
+      echo '<div>Unpublishable: '.$unpub .'</div>';
+    }
+    
+
+  } else {
+    // File doesn't exist in Cascade
+    file_put_contents('../indexes/stu-media-missing.txt', $cascadePath."\n", FILE_APPEND);
+    echo '<div class="f">The '.$cascadePath.' Media in site '.$siteName.' is absent from Cascade.</div>';
+    $absent++;
+    echo '<div>Absent: '.$absent .'</div>';
+  }
+  // echo '<pre>';
+  // print_r($publishableFolders);
+  // echo '</pre>';
+  // if ( count($unpublishableFolders) > 0) {
+  //   echo '<pre>';
+  //   print_r($unpublishableFolders);
+  //   echo '</pre>';
+  // }
+}
+
+echo '<h2>Stats</h2>';
+echo '<div>Lines: '.count($lines) .'</div>';
+echo '<div>Read: '.$read .'</div>';
+echo '<div>Publishable: '.$publishable .'</div>';
+echo '<div>OK: '.$ok .'</div>';
+echo '<div>Unpublishable: '.$unpub .'</div>';
+echo '<div>Absent: '.$absent .'</div>';
+echo '<div>Ignored: '.$ignored .'</div>';
+echo '<div>Unpublishable folder: '.$inUnpubFolder.'</div>';
+
+sort($publishableFolders);
+echo '<h2>Publishable Folders:</h2><pre>';
+print_r($publishableFolders);
+echo '</pre>';
+
+sort($unpublishableFolders);
+echo '<h2>Unpublishable Folders:</h2><pre>';
+print_r($unpublishableFolders);
+echo '</pre>';
+
+?>
