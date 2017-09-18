41d892471d9398e85391c2be7176bfec843a721d
Stu Basden <stu@tapiocacollective.com>
2017-09-18 10:30:09 +0100

    Clear up some occurrences of PHP7s' "PHP Notice:  Undefined index"

--- _cron.php
+++ _cron.php
@@ -4,6 +4,10 @@ include_once(__DIR__.'/rollbar-init.php');
 use \Rollbar\Rollbar;
 use \Rollbar\Payload\Level;
 
+foreach(['after','before','children'] as $key) {
+  if ( empty($_POST[$key]) ) {$_POST[$key] = false;}
+}
+
 parse_str(implode('&', array_slice($argv, 1)), $_GET);
 
 $o = array('','','','','');
